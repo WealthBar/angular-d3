b08e19c2924a7d1d1eec7c7bcd40d3f29af6606a
Chris Nicola <chnicola@gmail.com>
2013-08-04 17:05:57 -0700

    Rebuilding docs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,24 +6,18 @@
       controller: 'MainCtrl'
     });
     return $locationProvider.html5Mode(true);
-  }).directive('tab', function($rootScope, $location) {
-    return function(scope, element, attrs) {
-      var hash, toggleActive, watch;
-      hash = attrs.href;
-      watch = function() {
-        return $location.hash();
-      };
-      toggleActive = function(value) {
-        return element.parent().toggleClass("active", value === hash.slice(1));
-      };
-      return $rootScope.$watch(watch, toggleActive);
-    };
   });
 
 }).call(this);
 
 //@ sourceMappingURL=main.map
 (function() {
-  angular.module('angularD3App').controller('MainCtrl', function($scope) {});
+  angular.module('angularD3App').controller('MainCtrl', function($scope, $timeout) {});
+
+}).call(this);
+
+//@ sourceMappingURL=list-group.map
+(function() {
+
 
 }).call(this);
